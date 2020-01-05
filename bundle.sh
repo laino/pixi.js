f03049b1ac65e140e49a7f4660787e7db8f818d4
Nils Kuhnhenn <lain@volafile.org>
2020-01-05 15:24:53 +0100

    hopefully yarn can extract this properly now

--- bundle.sh
+++ bundle.sh
@@ -4,5 +4,4 @@ yarn build
 
 cd bundles/pixi.js
 
-tar -cvf ../../pixi.js.tar.gz --exclude node_modules/* .
-tar --append -vf ../../pixi.js.tar.gz node_modules/@pixi/*/*
+tar -cvf ../../pixi.js.tar.gz node_modules/@pixi/*/* --exclude node_modules/* .
